694367e301970fc7bafcc1ef3ebfa774307a11bb
Ilya Vlasov <Fox20073@users.noreply.github.com>
2020-10-26 21:59:15 -0400

    Mongoose
    Mongoose added and export
    
    Models.js createted for modules creation
    
    Index.js updated for new format and availability to use users and movies database

--- index.js
+++ index.js
@@ -3,62 +3,18 @@ const express = require('express'),
 
 const app = express();
 
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+
 app.use(morgan('common'));
 
 app.use(express.static('public'));
 
-let topMovies = [
-    {
-        title: 'Never Back Down',
-        director: 'Jeff Wadlow',
-        year: 2008
-    },
-    {
-        title: 'Step Up',
-        director: 'Anne Fletcher',
-        year: 2006
-    },
-    {
-        title: 'The Fast and the Furious',
-        director: 'Rob Cohen',
-        year: 2001
-    },
-    {
-        title: 'Avatar',
-        director: 'James Cameron',
-        year: 2009
-    },
-    {
-        title: 'Fight Club',
-        director: 'David Fincher',
-        year: 1999
-    },
-    {
-        title: 'Titanic',
-        director: 'James Cameron',
-        year: 1997
-    },
-    {
-        title: 'Back to the Future',
-        director: 'Robert Zemeckis',
-        year: 1985
-    },
-    {
-        title: 'Forest Gump',
-        director: 'Robert Zemeckis',
-        year: 1994
-    },
-    {
-        title: 'Ghostbusters',
-        director: 'Ivan Reitman',
-        year: 1984
-    },
-    {
-        title: 'Gladiator',
-        director: 'Ridley Scott',
-        year: 2000
-    }
-];
+const Movies = Models.Movies;
+const Users = Models.Users;
+
+mongoose.connect('mongodb://localhost:27017/moovivdb', { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 app.get('/', (req, res) =>{
     res.send('Welcome to MooVIV page!');
@@ -66,50 +22,190 @@ app.get('/', (req, res) =>{
 
 //GET all movies
 
-app.get('/movie', (req, res) => {
-    res.send('Successful GET request returning data on all movies');
+app.get('/movies', (req, res) => {
+    Movies.find()
+        .then((Movies) => {
+            res.status(201).json(Movies);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error: " + err);
+        });
+});
+
+// Get all users
+
+app.get('/users', (req, res) => {
+  Users.find()
+    .then((Users) => {
+      res.status(201).json(Users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+// Get User bu Username
+
+app.get('/users/:Username', (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((Users) => {
+      res.json(Users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
 //GET Movie by Title
 
 app.get('/movie/:title', (req, res) =>{
-    res.send('Successful GET request returning data on movie title: ' + req.params.title);
+    Movies.findOne({ Title:req.params.Title})
+        .then((Movies) => {
+            res.status(201).json(Movies)
+        })
+        .catch((err) =>{
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
 });
 
-//GET info by Director
+//GET info about Director
 
-app.get('/movie/:director', (req, res) => {
-    res.send('Successful GET request returning data on director :'+ req.params.director);
+app.get('/movie/Director/:Name', (req, res) => {
+    Movies.findOne({ "Director.Name" : req.params.Name})
+    .then((Movies) => {
+        res.status(201).json(Movies.Director.Name + ": " + Movies.Director.Bio);
+    })
+    .catch((err) =>{
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+    });
 });
 
 //POST new user
 
-app.post('/user', (req, res) =>{
-    res.send('Successful POST request registering new user');
+app.post('/users', (req, res) => {
+  Users.findOne({ Username: req.body.Username })
+    .then((Users) => {
+      if (Users) {
+        return res.status(400).send(req.body.Username + 'already exists');
+      } else {
+        Users
+          .create({
+            Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+          .then((Users) =>{res.status(201).json(Users) })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send('Error: ' + error);
+        })
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
 });
 
 // PUT updates to users info
 
-app.put('/user/:name', (req, res) =>{
-    res.send('Successful PUT request to update users: '+ req.params.name +' info');
+app.put('/users/:Username', (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
+    {
+      Username: req.body.Username,
+      Password: req.body.Password,
+      Email: req.body.Email,
+      Birthday: req.body.Birthday
+    }
+  },
+  { new: true },
+  (err, updatedUsers) => {
+    if(err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUsers);
+    }
+  });
 });
 
 // POST new movie to fav list
 
-app.post('/user/:name/movie/:title', (req, res) =>{
-    res.send('Successful POST request to add movie '+ req.params.title +' too '+ req.params.name + ' favorite list');
+app.post('/users/:Username/Movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, {
+     $push: { FavoriteMovies: req.params.MovieID }
+   },
+   { new: true },
+  (err, updatedUsers) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUsers);
+    }
+  });
 });
+
+// Put updates to user information
+app.put('/users/:Username', (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username},
+    { $set: {
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthdate: req.body.Birthdate
+      }
+    },
+    {new: true},
+    (err, updatedUsers) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      } else {
+        res.status(201).json(updatedUsers);
+      }
+    });
+  });
 
 // DELETE movie from fav
 
-app.delete('/user/:name/movie/:title', (req, res) =>{
-    res.send('Successful DELETE request. movie '+ req.params.title + ' was deleted from '+ req.params.name +' favorite list');
-});
+app.delete('/users/:Username/Favorites/:MovieID', (req, res) =>{
+       Users.findOneAndUpdate(  
+         { Username: req.params.Username},
+      { $pull: { FavoriteMovies: req.params.MovieID}
+    },
+    {new: true},
+     (err, updatedUsers) => {
+      if(err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+     } else {
+       res.json(updatedUsers);
+     }
+   });
+ });
 
 // DELETE users account
 
-app.delete('/user/:name', (req, res) =>{
-    res.send('Successfully DELETE of '+ req.params.name + ' information from server');
+app.delete('/users/:Username', (req, res) => {
+  Users.findOneAndRemove({ Username: req.params.Username })
+    .then((Users) => {
+      if (!Users) {
+        res.status(400).send(req.params.Username + ' was not found');
+      } else {
+        res.status(200).send(req.params.Username + ' was deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
 app.use((err, req, res, next) => {
---
+++ models.js
@@ -0,0 +1,31 @@
+const mongoose = require ("mongoose");
+
+let MoviesSchema = mongoose.Schema({
+  Title: { type: String, required: true },
+  Description: { type: String, required: true },
+  Genre: {
+    Name: String,
+    Description: String,
+  },
+  Director: {
+    Name: String,
+    Bio: String,
+  },
+  Actors: [String],
+  ImagePath: String,
+  Featured: Boolean,
+});
+
+let UsersSchema = mongoose.Schema({
+  Username: {type: String, required: true},
+  Password: {type: String, required: true},
+  Email: {type: String, required: true},
+  Birthday: Date,
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+});
+
+let Movies = mongoose.model("Movies", MoviesSchema);
+let Users = mongoose.model("Users", UsersSchema);
+
+module.exports.Movies = Movies;
+module.exports.Users = Users;
